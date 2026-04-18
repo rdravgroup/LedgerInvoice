2803072bfce9497b715b0e81b45bc9125a6bf81b
Sanju <rdravgroup@gmail.com>
2026-04-18 16:34:27 +0530

    APIhttpse

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://sanjulogin.runasp.net/api/'  // Production API URL
+  apiUrl: 'https://sanjulogin.runasp.net/api/'  // Production API URL
 };
